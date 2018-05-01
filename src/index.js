8f8d260e588d155fd0722b459c21233df3281736
Marc Ward <mijward.cs@gmail.com>
2018-05-01 05:18:52 -0400

    create-react-app calculator

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+//import '../node_modules/bootstrap/dist/js/bootstrap';
+import Calculator from './Calculator';
+
+ReactDOM.render(<Calculator />, document.getElementById('root'));
+
